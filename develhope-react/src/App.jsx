993b4b8cd99f1bdd920dfe7b29146be73600f8d9
ktatoglan <k.tatoglan@gmail.com>
2023-10-28 22:36:10 +0300

    strong tag

--- develhope-react/src/App.jsx
+++ develhope-react/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
       <div className="App">
         <h1>Hello, World!</h1>
-        <Welcome name={<i>Kazim</i>} age="29" />
+        <Welcome name={<strong>Kazim</strong>} age="29" />
       </div>
   )
 }
