6c13eb5830c35aa9cf18156b1f683e55a0daf533
ktatoglan <k.tatoglan@gmail.com>
2023-10-28 22:35:15 +0300

    19-Props

--- develhope-react/src/App.jsx
+++ develhope-react/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
       <div className="App">
         <h1>Hello, World!</h1>
-        <Welcome name="Kazim" age="29" />
+        <Welcome name={<i>Kazim</i>} age="29" />
       </div>
   )
 }
