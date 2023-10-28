a5178e10ef41a5cac21185fa1acc7ec8a6ceedec
ktatoglan <k.tatoglan@gmail.com>
2023-10-28 22:40:45 +0300

    20-Props

--- develhope-react/src/Welcome.jsx
+++ develhope-react/src/Welcome.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Age from './Age';
 
 function Welcome({ name,age }) {
   return (
     <>
         <p>Welcome, {name}!</p>
-        <p>Your age is {age}.</p>
+        <Age age={age} />
     </>
   );
 }
