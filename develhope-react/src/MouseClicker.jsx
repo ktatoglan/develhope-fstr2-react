d8f8c485215a5b7c30ea07c8ec6dd897aa286118
ktatoglan <k.tatoglan@gmail.com>
2023-10-29 17:15:07 +0300

    38-HandlingEvents

--- develhope-react/src/MouseClicker.jsx
+++ develhope-react/src/MouseClicker.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function MouseClicker({ name }) {
   const handleClick = (event) => {
     console.log(name);
   };
 
+  const handleImageClick = (event) => {
+    console.log(event.target.getAttribute("src"));
+  };
+
   return (
     <div>
-       <button name={name} onClick={handleClick}>
-        <img src="https://placehold.co/600x400" alt="Button Image" />
+      <button name={name} onClick={handleClick}>
+        <img
+          src="https://placehold.co/600x400"
+          onClick={handleImageClick}
+          alt="Button Image"
+        />
       </button>
     </div>
   );
