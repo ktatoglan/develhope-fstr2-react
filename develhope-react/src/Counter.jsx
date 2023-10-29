9c180d56e1c23dd2aeb34f2d1697adf2ed788a68
ktatoglan <k.tatoglan@gmail.com>
2023-10-29 17:00:14 +0300

    34-useEffect

--- develhope-react/src/Counter.jsx
+++ develhope-react/src/Counter.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CounterDisplay from './CounterDisplay';
 
 function Counter({ initialValue, incrementAmount, decrementAmount }) {
   const [counter, setCounter] = useState(initialValue);
+
   const incrementCounter = () => {
     setCounter(counter + incrementAmount);
   };
+
   const decrementCounter = () => {
     setCounter(counter - decrementAmount);
   };
+
   const resetCounter = () => {
     setCounter(initialValue);
   };
+
+  useEffect(() => {
+    console.log(`Current Counter Value: ${counter}`);
+  }, [counter]);
 
   return (
     <div>
