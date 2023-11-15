763d4c81b1d437e8b37676e513eeed190c80db3e
ktatoglan <k.tatoglan@gmail.com>
2023-11-16 01:18:47 +0300

    StylingComponents5

--- develhope-react/src/TodoList.jsx
+++ develhope-react/src/TodoList.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
+import styles from './TodoList.module.scss';
 const TodoList = () => {
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
+  const [newItem, setNewItem] = useState('');
 
   const addItem = () => {
-    if (newItem.trim() !== "") {
+    if (newItem.trim() !== '') {
       setItems([...items, newItem]);
-      setNewItem("");
+      setNewItem('');
     }
   };
 
@@ -15,28 +15,24 @@ const TodoList = () => {
     setItems([]);
   };
 
-  const removeItem = (indexToRemove) => {
-    const updatedItems = items.filter((item, index) => index !== indexToRemove);
-    setItems(updatedItems);
-  };
-
   return (
-    <div>
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-        placeholder="Add a new item"
-      />
-      <button onClick={addItem}>Add</button>
-      <button onClick={resetItems}>Reset</button>
+    <div className={styles.todoList}>
+      <div className={styles.inputContainer}>
+        <input
+          type="text"
+          className={styles.todoInput}
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          placeholder="Add a new item"
+        />
+        <button className={styles.addButton} onClick={addItem}>
+          Add
+        </button>
+        <button onClick={resetItems}>Reset</button>
+      </div>
       <ul>
         {items.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => removeItem(index)}>Remove</button>
-          </li>
+          <li key={index}>{item}</li>
         ))}
       </ul>
     </div>
