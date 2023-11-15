4725949f43c4008fef168426f7b458de31f9c042
ktatoglan <k.tatoglan@gmail.com>
2023-11-16 01:28:24 +0300

    StylingComponents7

--- develhope-react/src/FocusableInput.jsx
+++ develhope-react/src/FocusableInput.jsx
@@ -1,16 +1,28 @@
 import React, { useRef, useEffect } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const FocusableInput = () => {
   const inputRef = useRef(null);
 
   useEffect(() => {
-    // Focus on the input element when the component mounts
     inputRef.current.focus();
-  }, []); // Empty dependency array ensures this effect runs only once after initial render
+  }, []);
 
   return (
-    <div>
-      <input type="text" ref={inputRef} />
+    <div className="container mt-4">
+      <Form>
+        <Form.Group>
+          <Form.Label>Label</Form.Label>
+          <Form.Control type="text" placeholder="placeholder" ref={inputRef} />
+          <Form.Text className="text-muted">
+            text-muted
+          </Form.Text>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
