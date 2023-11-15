ecab65acc8dac0597b9b7b32c7fed1d99621ccba
ktatoglan <k.tatoglan@gmail.com>
2023-11-16 00:17:21 +0300

    ControlledForms2

---
+++ develhope-react/src/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRememberMeChange = (event) => {
+    setRememberMe(event.target.checked);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Process login logic here using 'username', 'password', and 'rememberMe'
+    // For example: Send login request to server or perform client-side validation
+    console.log('Submitted:', { username, password, rememberMe });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="rememberMe">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            checked={rememberMe}
+            onChange={handleRememberMeChange}
+          />
+          Remember Me
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
