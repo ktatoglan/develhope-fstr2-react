cbd1fd211bd50f3427953de1db9e6f34828acff2
ktatoglan <k.tatoglan@gmail.com>
2023-11-16 01:23:16 +0300

    StylingComponents6

--- develhope-react/src/UncontrolledLogin.jsx
+++ develhope-react/src/UncontrolledLogin.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useRef } from 'react';
 
-const UncontrolledForm = ({ onSubmit }) => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
+const UncontrolledLogin = ({ onLogin }) => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
 
-    const formData = new FormData(event.target);
-    for (const [name, value] of formData.entries()) {
-      console.log(`${name}: ${value}`);
-    }
-    onSubmit(formData);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    onLogin({ username, password });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
+    <form onSubmit={handleLogin} className="max-w-xs mx-auto mt-8">
+      <div className="mb-4">
+        <label htmlFor="username" className="block mb-1 text-sm font-semibold">
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          ref={usernameRef}
+          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+        />
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+      <div className="mb-4">
+        <label htmlFor="password" className="block mb-1 text-sm font-semibold">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          ref={passwordRef}
+          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+        />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        Login
+      </button>
     </form>
   );
 };
 
-export default UncontrolledForm;
+export default UncontrolledLogin;
