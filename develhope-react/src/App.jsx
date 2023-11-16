cd768ba2d66ff54152119734a0fb393b3478c7fe
ktatoglan <k.tatoglan@gmail.com>
2023-11-16 23:24:44 +0300

    Update App.jsx

--- develhope-react/src/App.jsx
+++ develhope-react/src/App.jsx
@@ -3,11 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GithubUsers from "./GithubUsers";
 import ShowGithubUser from "./ShowGithubUser";
 
+const IndexMessage = () => {
+  return <h3>Add a user and select it</h3>;
+};
+
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<GithubUsers />} />
+        <Route path="/" element={<GithubUsers />} />
+        <Route path="/" exact element={<IndexMessage />} />
         <Route path="/users/:username" element={<ShowGithubUser />} />
       </Routes>
     </Router>
