a8f15ef63d032a29234e960daef6f2a4cc2dbbd2
ktatoglan <k.tatoglan@gmail.com>
2023-11-16 16:31:59 +0300

    Update Container.jsx

--- develhope-react/src/Container.jsx
+++ develhope-react/src/Container.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Container = ({ children, title }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <div className="bg-white border border-red-500 p-4">
-      {title && <h2 className="text-red-500 text-xl mb-4">{title}</h2>}
-      {children}
+      {title && (
+        <h2
+          className="text-red-500 text-xl mb-4 cursor-pointer"
+          onClick={toggleCollapse}
+        >
+          {title}
+        </h2>
+      )}
+      {!collapsed && children}
     </div>
   );
 };
